eb1f3ba5e6a0c418557e092ba244089a9a971590
chwello <fretchelm.gerarman@gmail.com>
2024-04-06 21:55:56 +0800

    labyu

--- src/app/(private)/dashboard/page.jsx
+++ src/app/(private)/dashboard/page.jsx
@@ -1,6 +1,49 @@
+import { Card } from "antd";
+import { Space, Table, Tag } from "antd";
+
+
 function DashboardPage() {
   return (
-    <div>DashboardPage</div>
-  )
+    <>
+  
+      <Card className="h-40 w-[54%] flex items-center rounded-md border border-[#E6EDFF] font-sans">
+        <div className="flex items-center">
+          <div className="flex items-center flex-col mr-8 p-6">
+            <h1 className="mr-2 font-semibold text-3xl">300</h1>
+            <p className="text-sm whitespace-nowrap">Total votes</p>
+          </div>
+          <div className="ml-8 mb-10">
+            <img className="w-24 h-24" src="icon.svg" alt="users" />
+          </div>
+          <hr className="h-30 w-28 transform rotate-90 bg-[#E6EDFF]" />
+          <div className="flex items-center flex-col ml-8">
+            <h1 className="font-semibold text-3xl mr-auto">0</h1>
+            <p className="text-sm whitespace-nowrap">Registered Voters</p>
+          </div>
+          <div className="ml-10 mb-10">
+            <img className="w-24 h-24" src="registervoter.svg" alt="register" />
+          </div>
+          <hr className="h-30 w-28 transform rotate-90 bg-[#E6EDFF]" />
+        </div>
+      </Card>
+
+      <Card className="mt-6 rounded-md border border-[#E6EDFF] flex fontFamily-sans">
+        <div>
+          <div className="flex items-center ">
+            <h1 className="font-bold text-lg">Ranking</h1>
+            <p className="text-[#7C8DB5] text-[12px] mr-auto">12:30 PM, May 7</p>
+          </div>
+          <div className="flex justify-center text-center m-auto">
+            <h4>Governor</h4>
+          </div>
+          <Table>
+          
+       
+    
+          </Table>
+        </div>
+      </Card>
+    </>
+  );
 }
-export default DashboardPage
+export default DashboardPage;
--- src/app/(private)/layout.jsx
+++ src/app/(private)/layout.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import '../globals.css'
+import '../globals.css';
 import {
   DesktopOutlined,
   FileOutlined,
@@ -8,8 +8,10 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+
 const { Header, Content, Footer, Sider } = Layout;
+
 function getItem(label, key, icon, children) {
   return {
     key,
@@ -18,56 +20,76 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
-const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
+
+const items1 = [
+  getItem('Dashboard', '1', <img className="w-4 h-4" src="chart.svg" alt="chart" /> ),
+  getItem('Candidates', 'sub1', <img className="w-4 h-4" src="candidates.svg" alt="chart" />, [
+    getItem('Atup', '3'),
+    getItem('DibDib', '4'),
   ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+  getItem('Vote', '9', <img className="w-4 h-4" src="voters.svg" alt="chart" />),
 ];
+
+const items2 = [
+  getItem('Help Centre', '2', <img className="w-4 h-4" src="help-center.svg" alt="chart" />),
+  getItem('Contact us', '3', <img className="w-4 h-4" src="contactus.svg" alt="chart" />),
+  getItem('Log out', '4', <img className="w-4 h-4" src="logout.svg" alt="chart"  />),
+];
+
 const PrivateLayout = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
   return (
-    <Layout
-      className='h-screen'
-    >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} className='bg-white text-black'>
+    <Layout className='h-screen bg-[#FAFBFC]'>
+      
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} style={{ backgroundColor: 'white' }} className='text-[#7C8DB5] w-[200px]'>
+        <img className='pt-14 pl-4 w-32 h-34 self-center' src="edulogo.svg" alt="logo-eduvote" />
         <div className="demo-logo-vertical" />
-        <Menu defaultSelectedKeys={['1']} mode="inline" items={items} className='bg-white text-black'/>
+        
+        {/* First Menu */}
+        <Menu defaultSelectedKeys={['1']} mode="inline" items={items1} style={{ backgroundColor: 'white' }} className='font-sans pt-9' />
+
+        {/* Second Menu */}
+        <Menu defaultSelectedKeys={['']} mode="inline" items={items2} style={{ backgroundColor: 'white',  }} className='pt-80 font-sans' />
+        <style>
+          {`
+            .ant-menu-item-selected {
+              background-color: transparent !important;
+            }
+
+            .ant-layout-sider-trigger {
+              display: none !important;
+            }
+
+           
+            
+          `}
+        </style>
       </Sider>
+      
       <Layout>
+      
         <Header
           style={{
+            height: 90,
             padding: 0,
             background: colorBgContainer,
           }}
+          
         />
         <Content
           style={{
             margin: '0 16px',
           }}
         >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
           <div
             style={{
               padding: 24,
               minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
+             
             }}
           >
             {children}
@@ -84,4 +106,5 @@ const PrivateLayout = ({ children }) => {
     </Layout>
   );
 };
+
 export default PrivateLayout;
