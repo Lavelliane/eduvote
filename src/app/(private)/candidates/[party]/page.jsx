84b34ad8c7473576cdb83e8a7f22b47ccaa54109
Lavelliane <jhurylastre432@gmail.com>
2024-04-13 22:34:33 +0800

    get party by id query

--- src/app/(private)/candidates/[party]/page.jsx
+++ src/app/(private)/candidates/[party]/page.jsx
@@ -1,22 +1,23 @@
-import { useMemo } from 'react'
+'use client'
+import { useEffect, useMemo } from 'react'
 import { getPartyById} from '@/lib/queries/party/getPartyById'
+import { Card } from 'antd'
+import { useQuery } from '@tanstack/react-query'
 
 //KITAKITS SA APRIL 27
 
 // ================ I LOVE YOU ======================
-// export default function PartyPage({ params }){
+export default function PartyPage({ params }){
 
-//   const partyId = useMemo(() => {
-//     return params.party
-//   }, [params]);
+  const partyId = useMemo(() => {
+    return params.party
+  }, [params]);
 
   //Use useQuery to get data about the party. Use partyId and getPartyById function
-
-
-  export default function partyId({ }){
-
-    
-
+  const { data: party, isLoading } = useQuery({
+    queryKey: ['party', partyId],
+    queryFn: () => getPartyById(partyId)
+  })
 
 
   //Once party is present, render the component
