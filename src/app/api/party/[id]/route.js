e834a592b76a6d527a3de327e23d0c377eed8163
Lavelliane <jhurylastre432@gmail.com>
2024-05-01 00:23:39 +0800

    added comments

--- src/app/api/party/[id]/route.js
+++ src/app/api/party/[id]/route.js
@@ -3,9 +3,12 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
 //params is url params e.g. /api/party/12345?tab=2 .. 12345 is params.id
+// access the party id using params
 export async function GET(req, { params }) {
   try {
+    // store party id into variable
     const partyId = params.id
+    // use find unique with ID as the filter. Include candidates
     const party = await prisma.party.findUnique({
       where: { id: partyId },
       include: {
--- src/app/api/user/count/route.js
+++ src/app/api/user/count/route.js
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
+//API to get the count of all VOTERS (not including admin)
 export async function GET(req) {
   try {
     const count = await prisma.user.count({ where: { role: 'VOTER' } })
